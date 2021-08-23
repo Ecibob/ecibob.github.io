aae053231b4dbb98fe64c88f63937ddbf00e6844
Ecibob <keithariebecker@gmail.com>
2021-08-22 21:33:01 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// FLIP CARDS WORKSHOP PAGE
+const cards = document.querySelectorAll('.card');
+
+function transition() {
+  if (this.classList.contains('activeFlip')) {
+    this.classList.remove('activeFlip');
+  } else {
+    this.classList.add('activeFlip');   
+  }
+}
+
+cards.forEach(card => card.addEventListener('click', transition));
+
+// TOP NAV SLIDER
+$(document).ready(function () {
+
+  $(".menuItem").hover(function () {
+    var pos = $(this).attr("alt");
+    $("#slider").stop().animate({
+      marginLeft: 300 * (pos - 1),
+    }, 300)
+
+  });
+
+})
