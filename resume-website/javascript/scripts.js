9964437c70b39df30636460cb9281644e68b4355
Ecibob <keithariebecker@gmail.com>
2021-09-06 14:43:33 -0500

    rev2

---
+++ resume-website/javascript/scripts.js
@@ -0,0 +1,54 @@
+var $gear1 = $('.gear1'),
+    $gear2 = $('.gear2'),
+    $gear3 = $('.gear3'),
+    $gear4 = $('.gear4'),
+    $body = $('body'),
+    bodyHeight = $body.height();
+
+function getScrollTop() {
+    if (typeof pageYOffset != 'undefined') {
+        //most browsers except IE before #9
+        return pageYOffset;
+    } else {
+        var B = document.body; //IE 'quirks'
+        var D = document.documentElement; //IE with doctype
+        D = (D.clientHeight) ? D : B;
+        return D.scrollTop;
+    }
+}
+
+$(window).scroll(function () {
+    var scroll = getScrollTop();
+    $gear1.css({
+        'transform': 'rotate(' + (scroll / bodyHeight * 800) + 'deg)',
+        '-moz-transform': 'rotate(' + (scroll / bodyHeight * 800) + 'deg)',
+        '-ms-transform': 'rotate(' + (scroll / bodyHeight * 800) + 'deg)',
+        '-o-transform:rotate': 'rotate(' + (scroll / bodyHeight * 800) + 'deg)'
+
+    });
+    $gear2.css({
+        'transform': 'rotate(' + (scroll / bodyHeight * ("-300")) + 'deg)',
+        '-moz-transform': 'rotate(' + (scroll / bodyHeight * ("-1000")) + 'deg)',
+        '-ms-transform': 'rotate(' + (scroll / bodyHeight * ("-1000")) + 'deg)',
+        '-o-transform:rotate': 'rotate(' + (scroll / bodyHeight * ("-1000")) + 'deg)'
+
+    });
+    $gear3.css({
+        'transform': 'rotate(' + (scroll / bodyHeight * 300) + 'deg)',
+        '-moz-transform': 'rotate(' + (scroll / bodyHeight * ("-1000")) + 'deg)',
+        '-ms-transform': 'rotate(' + (scroll / bodyHeight * ("-1000")) + 'deg)',
+        '-o-transform:rotate': 'rotate(' + (scroll / bodyHeight * ("-1000")) + 'deg)'
+    });
+    $gear4.css({
+        'transform': 'rotate(' + (scroll / bodyHeight * ("-800")) + 'deg)',
+        '-moz-transform': 'rotate(' + (scroll / bodyHeight * ("-1000")) + 'deg)',
+        '-ms-transform': 'rotate(' + (scroll / bodyHeight * ("-1000")) + 'deg)',
+        '-o-transform:rotate': 'rotate(' + (scroll / bodyHeight * ("-1000")) + 'deg)'
+    });
+});
+
+$(document).ready(function () {
+    $(".toggle").on('click', function () {
+        $(".toggle-content").slideToggle();
+    });
+});
